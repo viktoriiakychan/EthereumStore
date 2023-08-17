e61f16bce56ba9f0285ab3daacc3435476ce9c73
k2 <trofimchuk.an@gmail.com>
2023-08-17 20:17:49 +0300

    Course card

--- pages/marketplace/index.js
+++ pages/marketplace/index.js
@@ -1,38 +1,34 @@
+import { CourseList, CourseCard } from "@components/ui/course";
+import { BaseLayout } from "@components/ui/layout";
+import { getAllCourses } from "@content/courses/fetcher";
+import { WalletBar } from "@components/ui/web3";
+import { useAccount } from "@components/hooks/web3/useAccount";
+import { useNetwork } from "@components/hooks/web3/useNetwork";
 
+export default function Marketplace({ courses }) {
+  const { account } = useAccount();
+  const { network } = useNetwork();
 
-import { CourseList } from "@components/ui/course"
-import { BaseLayout } from "@components/ui/layout"
-import { getAllCourses } from "@content/courses/fetcher"
-import { WalletBar } from "@components/ui/web3"
-import { useAccount } from "@components/hooks/web3/useAccount"
-import { useNetwork } from "@components/hooks/web3/useNetwork"
-
-export default function Marketplace({courses}) {
-  const { account } = useAccount()
-  const { network } = useNetwork()
-
+  console.log(`network.data ${network.data}`);
   return (
     <>
       <div className="py-4">
-        { network.data }
-        <WalletBar
-          address={account.data}
-        />
+        <WalletBar address={account.data} network={network.data} />
       </div>
-      <CourseList
-        courses={courses}
-      />
+      <CourseList courses={courses}>
+        {(course) => <CourseCard key={course.id} course={course} />}
+      </CourseList>
     </>
-  )
+  );
 }
 
 export function getStaticProps() {
-  const { data } = getAllCourses()
+  const { data } = getAllCourses();
   return {
     props: {
-      courses: data
-    }
-  }
+      courses: data,
+    },
+  };
 }
 
-Marketplace.Layout = BaseLayout
+Marketplace.Layout = BaseLayout;
